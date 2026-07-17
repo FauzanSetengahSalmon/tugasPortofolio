07450172cfda14014ace3fbe27bf5488f4b69057
FauzanSetengahSalmon <ahmadfauzan201006@gmail.com>
2026-07-17 14:43:12 +0700

    add profile migration and model

---
+++ app/Models/Profile.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $fillable = [
+        'name',
+        'headline',
+        'bio',
+        'avatar',
+        'email',
+        'linkedin',
+        'github',
+    ];
+
+    public function experiences()
+    {
+        return $this->hasMany(Experience::class)->orderBy('order');
+    }
+
+    public function educations()
+    {
+        return $this->hasMany(Education::class)->orderBy('order');
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class)->orderBy('order');
+    }
+
+    public function skills()
+    {
+        return $this->hasMany(Skill::class)->orderBy('order');
+    }
+}
